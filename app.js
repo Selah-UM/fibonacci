89f693ce0804cc793215e1fbcfec7e26d06c6920
selah <konomi_20n1101220@nnn.ed.jp>
2020-09-08 20:12:28 +0900

    表示の仕方を変更

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 'use strict';
+// function fib(n){
+    // console.log(n + "のフィボナッチ数は××です");
+// }
+// for(let i = 1; i <= 40; i++){
+//     fib(i);
+// }
 const memo = new Map();
 memo.set(0,0);
 memo.set(1,1);
@@ -12,5 +18,5 @@ function fib(n){
 }
 const length = 40;
 for(let i = 0; i <= length; i++){
-    console.log(fib(i));
+    console.log(i + "のフィボナッチ数は" + fib(i) + "です");
 }
